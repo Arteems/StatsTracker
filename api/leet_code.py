94500db1aaff0dace095cfab64ece7b7142fac56
Arteems <artm_frolov00@mail.ru>
2025-04-10 21:08:04 +0200

    Добавление сайта LeetCode и обновление файлов

--- api/leet_code.py
+++ api/leet_code.py
@@ -4,12 +4,13 @@ import requests
 class UserNotFoundError(Exception):
     pass
 
-def get_user_info(username: str):
-    url = f"https://www.codewars.com/api/v1/users/{username}"
+def get_info_about_user(username: str):
+    url = f"https://leetcode-stats-api.herokuapp.com/{username}"
     response = requests.get(url)
 
     if response.status_code == 200:
         return response.json()
     raise UserNotFoundError(f"Пользователь '{username}' не найден.")
+
 
 
--- handlers.py
+++ handlers.py
@@ -1,7 +1,7 @@
 from bot import bot
 from telebot import types
-from api.codewars_api import get_user_info
-from api.hackerrank_api import get_info_about_user
+from api.codewars_api import get_user_info, UserNotFoundError
+from api.leet_code import get_info_about_user
 
 
 states_users = {}
@@ -12,7 +12,7 @@ def say_hello(message):
 
 @bot.message_handler(commands=["help"])
 def get_help(message):
-    bot.send_message(message.chat.id, "/start - приветствие,\n /help - помощь, \n /codewars - получить статистику пользователя codewars")
+    bot.send_message(message.chat.id, "/start - приветствие,\n /help - помощь, \n /get_statistic - Выбрать сайт для получения статистики пользователя")
 
 
 
@@ -20,7 +20,7 @@ def get_help(message):
 def button_message(message):
     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
     item_cdw=types.KeyboardButton("Codewars")
-    item_hkr=types.KeyboardButton("HackerRank")
+    item_hkr=types.KeyboardButton("LeetCode")
     markup.add(item_cdw, item_hkr)
     bot.send_message(message.chat.id, "Выберите что вам нужно...", reply_markup=markup)
 
@@ -29,25 +29,53 @@ def message_reply(message):
     user_id = message.chat.id
 
     if user_id in states_users:
-        if states_users[user_id] == "awaiting_username":
-            username = message.text
-            user_statistic = get_user_info(username)
-            rank_name = user_statistic.get('ranks', {}).get('overall', {}).get('name')
-            total = user_statistic.get('codeChallenges', {}).get("totalCompleted")
+        if states_users[user_id] == "awaiting_username_codewars":
+            try:
+                username = message.text
+                get_user_statistic = get_user_info(username)
+                rank_name = get_user_statistic.get('ranks', {}).get('overall', {}).get('name')
+                total = get_user_statistic.get('codeChallenges', {}).get("totalCompleted")
+                leader_board = get_user_statistic.get('leaderboardPosition')
 
+                bot.send_message(user_id, f"Получена статистика для пользователя: \n Юзернейм: {username}\n Ранг: {rank_name}\n Всего решено задач: {total}\n Позиция на доске лидеров: {leader_board}")
+            except UserNotFoundError as e:
+                bot.send_message(user_id, str(e))
 
-
-            bot.send_message(user_id, f"Получена статистика для пользователя: \n Юзернейм: {username}\n Ранг: {rank_name}\n Всего решено задач: {total}")
+            except Exception as e:
+                bot.send_message(user_id, f"Произошла ошибка: {str(e)}")
 
             del states_users[user_id]
             return
 
+        elif states_users[user_id] == "awaiting_username_leetcode":
+            try:
+                username = message.text
+                get_user_statistic = get_info_about_user(username)
+                ranking = get_user_statistic.get('ranking', {})
+                total_solved = get_user_statistic.get('totalSolved', {})
+                contribution_points = get_user_statistic.get('contributionPoints', {})
+                total_questions = get_user_statistic.get('totalQuestions', {})
+
+                bot.send_message(user_id, f"Получена статистика для пользователя: \n Юзернейм: {username}\n Ранг: {ranking}\n Всего решено задач: {total_solved}\n Баллы вклада: {contribution_points}\n Всего вопросов: {total_questions}")
+
+            except UserNotFoundError as e:
+                bot.send_message(user_id, str(e))
+
+            except Exception as e:
+                bot.send_message(user_id, f"Произошла ошибка: {str(e)}")
+
+            del states_users[user_id]
+            return
 
     if message.text == "Codewars":
-        bot.send_message(message.chat.id,"Введите имя пользователя для получения статистики:")
+        bot.send_message(user_id, "Введите имя пользователя для получения статистики:")
+        states_users[user_id] = "awaiting_username_codewars"
+        return
 
-        states_users[user_id] = "awaiting_username"
-
+    if message.text == "LeetCode":
+        bot.send_message(user_id, "Введите имя пользователя для получения статистики:")
+        states_users[user_id] = "awaiting_username_leetcode"
+        return
 
 # @bot.message_handler(commands=["/help"])
 # def help_user(message):
